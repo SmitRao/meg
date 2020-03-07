9c95ed0bce0e02df5c11b9b68972ae80abb14a0a
Smit Rao <smit.rao@mail.utoronto.ca>
2020-03-07 03:55:56 -0500

    Add how it works stuff

--- frontend/src/screens/HowItWorks/HowItWorks.js
+++ frontend/src/screens/HowItWorks/HowItWorks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HowItWorks.css";
-import { Layout, Menu, Typography, Card } from "antd";
+import { Layout, Menu, Typography } from "antd";
 import {
   SearchOutlined,
   FilterOutlined,
@@ -12,7 +12,7 @@ import {
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 const { Header, Content, Footer } = Layout;
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
 export class HowItWorks extends React.Component {
   render() {
@@ -33,17 +33,41 @@ export class HowItWorks extends React.Component {
           </Menu>
         </Header>
 
-        <Content style={{ padding: "0 50px" }}>
-          <div className="content">
-            <Card className="card">
-              <SearchOutlined></SearchOutlined>
-            </Card>
-            <Card className="card">
-              <FilterOutlined></FilterOutlined>
-            </Card>
-            <Card className="card">
-              <ShoppingCartOutlined></ShoppingCartOutlined>
-            </Card>
+        <Content className="content">
+          <div className="container">
+            <Title
+              strong
+              style={{ fontSize: "4em", justifyContent: "center" }}
+              className="title"
+            >
+              How it works
+            </Title>
+            <div className="cards-row-container">
+              <div className="card">
+                <SearchOutlined></SearchOutlined>
+                <Title level={3}>Search</Title>
+                <Text style={{ textAlign: "left", fontSize: "large" }}>
+                  Type what you’re looking for into the search bar and see
+                  results from verified clothing brands.
+                </Text>
+              </div>
+              <div className="card">
+                <FilterOutlined></FilterOutlined>
+                <Title level={3}>Filter</Title>
+                <Text style={{ textAlign: "left", fontSize: "large" }}>
+                  Use filters to narrow down your search and find the styles you
+                  love that align with he values that are important to you.
+                </Text>
+              </div>
+              <div className="card">
+                <ShoppingCartOutlined></ShoppingCartOutlined>
+                <Title level={3}>Find</Title>
+                <Text style={{ textAlign: "left", fontSize: "large" }}>
+                  Once you find that perfect piece, MEG redirects you to the
+                  vendor’s site to purchase that item.
+                </Text>
+              </div>
+            </div>
           </div>
         </Content>
 
