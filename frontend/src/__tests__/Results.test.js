3001d1bbe5281dc135905e3bdcbd20075a174bc7
bianca-pokhrel <biancaa.pokhrel@gmail.com>
2020-03-09 14:36:12 -0400

    seperated components and screens testing is done, need to fix an error in Results component

--- frontend/src/__tests__/Results.test.js
+++ frontend/src/__tests__/Results.test.js
@@ -3,21 +3,24 @@ import PriceFilter from '../components/Results/PriceFilter';
 import Products from '../components/Results/Products';
 import Sort from '../components/Results/Sort';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import '@testing-library/jest-dom/extend-expect';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure} from 'enzyme';
+configure({adapter: new Adapter()});
 
 
-/*
-it ('checks the price filter component', () => {
-    const price = TestRenderer.create(<PriceFilter />);
-    expect(price).toMatchSnapshot();
-});
-
-it ('checks the products component', () => {
-    const prod = TestRenderer.create(<Products />);
-    expect(prod).toMatchSnapshot();
-});
-*/
+describe('Testing Results components', () => {
+    it('price filter component check', () => {
+      const component = shallow(<PriceFilter/>);
+      expect(component).toMatchSnapshot();
+    });
+    
+    it('products component check', () => {
+        const component = shallow(<Products/>);
+      expect(component).toMatchSnapshot();
+    })
+  });
+  
 it ('checks the sort component', () => {
     const s = TestRenderer.create(<Sort />);
     expect(s).toMatchSnapshot();
--- frontend/src/__tests__/Screens.test.js
+++ frontend/src/__tests__/Screens.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@testing-library/jest-dom/extend-expect';
-import { render, fireEvent } from '@testing-library/react';
-import TestRenderer from 'react-test-renderer';
 import App from '../App';
 import Home from '../screens/Home/Home';
 import HowItWorks from '../screens/HowItWorks/HowItWorks';
 import Results from '../screens/Results/Results';
-import Navbar from '../components/Navbar/Navbar';
-import MegFooter from '../components/MegFooter/MegFooter';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure} from 'enzyme';
 configure({adapter: new Adapter()});
@@ -18,36 +14,23 @@ it('renders without crashing', () => {
     ReactDOM.render(<App />, div);
 });
 
-it('home page renders welcome message', () => {
-    const { getByText } = render(<App />);
-    expect(getByText('Finding ethically')).toBeInTheDocument();
+describe('testing splash page screen', () => {
+    it('should render correctly', () => {
+        const component = shallow(<Home />);
+        expect(component).toMatchSnapshot();
+    });
 });
 
-describe('testing Navbar', () => {
+describe('testing HowItWorks screen', () => {
     it('should render correctly', () => {
-      const component = shallow(<Navbar/>);
+      const component = shallow(<HowItWorks/>);
       expect(component).toMatchSnapshot();
     });
   });
 
-
-it('testing footer component', () => {
-    const foot = TestRenderer.create(<MegFooter />);
-    expect(foot).toMatchSnapshot();
-});
-
-it('renders the expected screen for the splash page', () => {
-    const home_screen = TestRenderer.create(Home).toJSON();
-    expect(home_screen).toMatchSnapshot();
-});
-
-it('renders the expected screen for the How it Works page', () => {
-    const hiw = TestRenderer.create(HowItWorks).toJSON();
-    expect(hiw).toMatchSnapshot();
-});
-
-it('renders the expected screen for the results page', () =>{
-    const results_screen = TestRenderer.create(Results).toJSON();
-    expect(results_screen).toMatchSnapshot();
-
+describe('testing Results screen', () => {
+    it('should render correctly', () => {
+        const component = shallow(<Results/>);
+        expect(component).toMatchSnapshot();
+    });
 });
