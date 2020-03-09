7bf8d529b35e4bafe2741a213c29d06acbe4f08a
limhyesu98 <redbaloon17@gmail.com>
2020-03-08 23:14:05 -0400

    clear database to ready to be merged with updated datbase folder structure

--- database/serching_queries.sql
+++
@@ -1,5 +0,0 @@
--- all keywords should be in lowercase
--- $search_keyword is a variable requested from client
-SELECT *
-FROM Products
-WHERE ProductName LIKE '%$search_keyword%' or ProductInEuros LIKE '%$search_keyword%' or Gender LIKE '%$search_keyword%' or ProductDetail LIKE '%$search_keyword%' or BrandName LIKE '%$search_keyword%' or CategoryName LIKE '%$search_keyword%';
--- database/sorting_queries.sql
+++
@@ -1,9 +0,0 @@
--- sort by price LOW to HIGH
-SELECT *
-FROM Products
-ORDER BY PriceInEuros ASC;
-
--- sort by price HIGH to LOW
-SELECT *
-FROM Products
-ORDER BY PriceInEuros DESC;
