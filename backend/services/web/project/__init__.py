36d55c49e079a517a87328983463d6a519fad3dc
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-08 23:51:53 -0400

    removed unused code

--- backend/services/web/project/__init__.py
+++ backend/services/web/project/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 from project import models
 
