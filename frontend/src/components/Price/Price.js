994435bc5f448a4fffdea6cffaa5ff9c31135b42
Janet Wang <jienet.wang@mail.utoronto.ca>
2020-03-28 12:33:44 -0400

    price conversion ok, will add currency symbols

--- frontend/src/components/Price/Price.js
+++ frontend/src/components/Price/Price.js
@@ -5,25 +5,46 @@ import {Typography} from "antd";
 
 const {Text} = Typography;
 
+const exchangeRates = require('./ExchangeRatesToCAD');
+
 class Price extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            curr: this.props
+            curr: this.validateCurrency(this.props.curr) ? this.props.curr : "USD",
+            currRate: this.getRate(this.props.curr)
         }
     }
 
-    convertValueByCurrency = (product_value, product_curr) => {
-        const rate = 0.90; // 0.90 is euro conversion
-        const val = product_value * rate;
-        // console.log(product_value, product_curr, val, this.state.curr);
-        return product_value * rate
+    // check valid currency we can convert
+    validateCurrency = (curr) => {
+        // console.log("validateCurrency", curr, curr in exchangeRates);
+        return curr in exchangeRates
+    };
+
+    // get exchange rate of curr to CAD
+    getRate = (curr) => {
+        if (this.validateCurrency(curr)) {
+            return exchangeRates[curr]
+        }
+        return exchangeRates["USD"]
+    };
+
+    // convert from value to the required currency
+    convertValuetoCurr = (value, curr) => {
+        // TODO: this method will only give an approximation!!!
+
+        const toCADRate = this.getRate(curr);
+        const conv = value * toCADRate / this.state.currRate;
+
+        // console.log(value, curr, conv, this.state.curr);
+        return conv.toFixed(2);
     };
 
     render() {
         const {product_value, product_curr} = this.props;
-        const {curr} = this.state;
-        const value = this.convertValueByCurrency( product_value, product_curr);
+        const curr = this.state.curr;
+        const value = this.convertValuetoCurr( product_value, product_curr);
 
         return <Text>{value} {curr}</Text>
     }
