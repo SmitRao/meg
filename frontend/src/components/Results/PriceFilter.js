0110a717f8f37c1ba2b1dbc92cbed18d9473f75a
lemicha3 <mich.le@mail.utoronto.ca>
2020-04-09 02:26:54 -0400

    reset filters button now includes clearing price filter text

--- frontend/src/components/Results/PriceFilter.js
+++ frontend/src/components/Results/PriceFilter.js
@@ -14,31 +14,55 @@ class PriceFilter extends React.Component {
     this.getMinMax = this.getMinMax.bind(this);
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.clear && prevProps.clear!==this.props.clear) {
+      this.setState({min: 0, max: 0})
+    }
+    if (this.props.currency !== prevProps.currency) {
+      this.setState({
+        min: convertValuetoCurr(this.state.min, prevProps.currency, this.props.currency),
+        max: convertValuetoCurr(this.state.max, prevProps.currency, this.props.currency)
+      });
+    }
+  }
+
   handleMinChange(event) {
-    const convertedMinVal = convertValuetoCurr(event.target.value, this.props.currency,"USD");
+      let value = parseFloat(event.target.value);
+      if (Number.isNaN(value)) {
+        value = 0;
+      }
     // console.log("handleMinChange", this.props.currency, event.target.value, "USD", convertedMinVal)
-    this.setState({min: convertedMinVal});
+      this.setState({min: value});
   }
 
   handleMaxChange(event) {
-    const convertedMaxVal = convertValuetoCurr(event.target.value, this.props.currency,"USD");
+    let value = parseFloat(event.target.value);
+    if (Number.isNaN(value)) {
+      value = 0;
+    }
     // console.log("handleMaxChange", this.props.currency, event.target.value, "USD", convertedMaxVal)
-    this.setState({max: convertedMaxVal});
+    this.setState({max: value});
   }
 
   getMinMax() {
-    this.props.onSet(this.state);
+    let values = {
+      min: convertValuetoCurr(this.state.min, this.props.currency, "USD"), 
+      max: convertValuetoCurr(this.state.max, this.props.currency, "USD")
+    }
+    this.props.onSet(values);
   }
 
   render() {
+    const minValue = this.state.min === 0 ? "" : this.state.min; 
+    const maxValue = this.state.max === 0 ? "" : this.state.max;
     return (
       <InputGroup size="small" className={this.props.className}>
         <Row justify="center" gutter={8}>
           <Col span={7}>
-            <Input placeholder="MIN" onChange={this.handleMinChange} maxLength={4} />
+            <Input placeholder="MIN" onChange={this.handleMinChange} id="minInput" maxLength={4} value={minValue}/>
           </Col>
           <Col span={7}>
-            <Input placeholder="MAX" onChange={this.handleMaxChange} maxLength={4} />
+            <Input placeholder="MAX" onChange={this.handleMaxChange} id="maxInput" maxLength={4} value={maxValue}/>
           </Col>
           <Col span={4}>
             <Button size="small" onClick={this.getMinMax}>SET</Button>
